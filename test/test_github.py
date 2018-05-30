a8658c63f4f06649df524e8a8f0b17ada3ab6704
Derek Howard <derek@howderek.com>
2018-05-29 19:47:08 -0500

    First git metric implemented end-to-end, performance improvements

--- test/test_github.py
+++ test/test_github.py
@@ -28,8 +28,8 @@ The tests check if a value is anywhere in the dataframe
 # *** ACTIVITY *** #
 
 # *** EXPERIMENTAL *** #
-def test_bus_factor(github):
-    assert github.bus_factor("OSSHealth", "augur",start="1-1-17", end="5-12-17").isin(["9"]).any
+# def test_bus_factor(github):
+#     assert github.bus_factor("OSSHealth", "augur",start="1-1-17", end="5-12-17").isin(["9"]).any
 
 # def test_tags(github):
 #     assert github.tags("OSSHealth", "augur").isin(["v0.2"]).any
