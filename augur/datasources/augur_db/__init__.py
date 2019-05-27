6723e624dfa16f8e57f4dd6d6cf1de9e534fcab0
gabe-heim <gabe.heim@yahoo.com>
2019-05-27 17:32:10 -0500

    New schema added as a datasource
    Just using old format of other datasources for now.

--- augur/datasources/augur_db/__init__.py
+++ augur/datasources/augur_db/__init__.py
@@ -8,23 +8,23 @@ class AugurPlugin(AugurPlugin):
     This plugin serves as an example as to how to load plugins into Augur
     """
     def __init__(self, augur):
-        self.__ghtorrent = None
+        self.__augur_db = None
         # _augur will be set by the super init
         super().__init__(augur)
 
     def __call__(self):
-        from .augur import Augur
-        if self.__augur is None:
+        from .augur_db import Augur
+        if self.__augur_db is None:
             logger.debug('Initializing Augur New Schema')
-            self.__ghtorrent = Augur(
+            self.__augur_db = Augur(
                 user=self._augur.read_config('Database', 'user', 'AUGUR_DB_USER', 'augur'),
-                password=self._augur.read_config('Database', 'pass', 'AUGUR_DB_PASS', 'password'),
+                password=self._augur.read_config('Database', 'password', 'AUGUR_DB_PASS', 'password'),
                 host=self._augur.read_config('Database', 'host', 'AUGUR_DB_HOST', '127.0.0.1'),
                 port=self._augur.read_config('Database', 'port', 'AUGUR_DB_PORT', '5433'),
                 dbname=self._augur.read_config('Database', 'name', 'AUGUR_DB_NAME', 'augur'),
                 schema=self._augur.read_config('Database', 'schema', 'AUGUR_DB_SCHEMA', 'augur_data')
             )
-        return self.__augur
+        return self.__augur_db
 
     def create_routes(self, flask_app):
         """
@@ -33,10 +33,10 @@ class AugurPlugin(AugurPlugin):
         from .routes import create_routes
         create_routes(flask_app)
 
-GHTorrentPlugin.augur_plugin_meta = {
-    'name': 'augur',
+AugurPlugin.augur_plugin_meta = {
+    'name': 'augur_db',
     'datasource': True
 }
-Application.register_plugin(Augur)
+Application.register_plugin(AugurPlugin)
 
 __all__ = ['AugurPlugin']
