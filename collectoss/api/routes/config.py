c210c10cf8fcd0d8f311aad2051ac86718ea03ba
Ulincsys <28362836a@gmail.com>
2023-04-17 10:31:49 -0500

    Admin updates
    - Add ssl decorator to config endpoints
    - Fix syntax error in admin_required decorator
    - Update dashboard endpoint to use config class directly
    - Update dashboard styles with more consistent colors
    - Implement config update functionality in admin dashboard
    
    Signed-off-by: Ulincsys <28362836a@gmail.com>

--- collectoss/api/routes/config.py
+++ collectoss/api/routes/config.py
@@ -9,13 +9,13 @@ import sqlalchemy as s
 # Disable the requirement for SSL by setting env["AUGUR_DEV"] = True
 from collectoss.application.config import get_development_flag
 from collectoss.application.db.lib import get_session
+from collectoss.api.util import ssl_required
 from collectoss.application.db.models import Config
 from collectoss.application.config import SystemConfig
 from collectoss.application.db.session import DatabaseSession
 from ..server import app
 
 logger = logging.getLogger(__name__)
-development = get_development_flag()
 
 from collectoss.api.routes import API_VERSION
 
@@ -28,6 +28,7 @@ def generate_upgrade_request():
     return response, 426
 
 @app.route(f"/{API_VERSION}/config/get", methods=['GET', 'POST'])
+@ssl_required
 def get_config():
     if not development and not request.is_secure:
         return generate_upgrade_request()
@@ -40,10 +41,8 @@ def get_config():
 
 
 @app.route(f"/{API_VERSION}/config/update", methods=['POST'])
+@ssl_required
 def update_config():
-    if not development and not request.is_secure:
-        return generate_upgrade_request()
-
     update_dict = request.get_json()
 
     with get_session() as session:
@@ -64,5 +63,3 @@ def update_config():
         session.commit()
 
     return jsonify({"status": "success"}), 200
-
-
