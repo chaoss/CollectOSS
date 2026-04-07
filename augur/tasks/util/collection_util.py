04e4c884b0733ccb7bbad844a655cf0c7d9f4bd2
Adrian Edwards <adredwar@redhat.com>
2026-04-07 17:38:50 -0400

    augur_collection_sequence -> collection_sequence
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- augur/tasks/util/collection_util.py
+++ augur/tasks/util/collection_util.py
@@ -525,15 +525,15 @@ class CollectionTaskRoutine:
                         continue
                     phases = col_hook.gitlab_phases
            
-                augur_collection_sequence = []
+                collection_sequence = []
                 for job in phases:
                     #Add the phase to the sequence in order as a celery task.
                     #The preliminary task creates the larger task chain 
-                    augur_collection_sequence.append(job(repo_git, full_collection))
+                    collection_sequence.append(job(repo_git, full_collection))
 
-                #augur_collection_sequence.append(core_task_success_util.si(repo_git))
+                #collection_sequence.append(core_task_success_util.si(repo_git))
                 #Link all phases in a chain and send to celery
-                augur_collection_chain = chain(*augur_collection_sequence)
+                augur_collection_chain = chain(*collection_sequence)
                 task_id = augur_collection_chain.apply_async().task_id
 
                 self.logger.info(f"Setting {platform_name} repo {col_hook.name} status to collecting for repo: {repo_git}")
