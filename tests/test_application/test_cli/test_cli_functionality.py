d5c131fcaf53c576d5345e4b9eaab0645045ddd8
Adrian Edwards <adredwar@redhat.com>
2026-04-18 21:21:42 -0400

    another use of the workers scripts
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- tests/test_application/test_cli/test_cli_functionality.py
+++ tests/test_application/test_cli/test_cli_functionality.py
@@ -21,7 +21,7 @@ def test_main_dependency_install():
 def test_data_analysis_dependency_installs():
 
     FNULL = open(os.devnull, "w")
-    command = "scripts/install/workers.sh $target 2>&1 | tee logs/workers-install.log"
+    command = "scripts/ci/workers.sh $target 2>&1 | tee logs/workers-install.log"
     process = subprocess.run(command.split(" "), stdout=FNULL, stderr=subprocess.STDOUT)
 
     assert process.returncode == 0
