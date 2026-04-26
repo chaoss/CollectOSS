5e15927ca6a56e21f2153e103bdeef734edca4e2
Adrian Edwards <adredwar@redhat.com>
2026-04-26 10:52:49 -0400

    update release package name and author
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- metadata.py
+++ metadata.py
@@ -1,13 +1,13 @@
 #SPDX-License-Identifier: MIT
-__name__ = "augur"
-__slug__ = "augur"
+__name__ = "collectoss"
+__slug__ = "collectoss"
 __url__ = "https://github.com/chaoss/collectoss"
 
 __short_description__ = "Python 3 package for free/libre and open-source software community metrics, models & data collection"
 
 __version__ = "0.92.0"
 __release__ = "v0.92.0 (Paladin Penguin)"
-__author__ = "CollectOSS Team"
+__author__ = "CollectOSS Community"
 
 __license__ = "MIT"
 __copyright__ = "University of Missouri, Derek Howard, Sean Goggins, Augurlabs 2025"
