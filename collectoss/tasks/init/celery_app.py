c72520e3b85a85e43f3eab0ef07df5e02591373e
Adrian Edwards <adredwar@redhat.com>
2026-05-08 16:28:10 -0400

    replace references to os.environ.get
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/tasks/init/celery_app.py
+++ collectoss/tasks/init/celery_app.py
@@ -63,7 +63,7 @@ frontend_tasks = ['collectoss.tasks.frontend']
 
 tasks = start_tasks + github_tasks + gitlab_tasks + git_tasks + materialized_view_tasks + frontend_tasks
 
-if os.environ.get('AUGUR_DOCKER_DEPLOY') != "1":
+if SystemEnv.get('AUGUR_DOCKER_DEPLOY') != "1":
     tasks += data_analysis_tasks
 
 redis_db_number, redis_conn_string = get_redis_conn_values()
