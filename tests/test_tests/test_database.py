272a5743e73e2c85f70860112a50108bc2e035ce
Adrian Edwards <adredwar@redhat.com>
2026-05-19 14:53:03 -0400

    update database unit test self-tests to use the new fixtures

--- tests/test_tests/test_database.py
+++ tests/test_tests/test_database.py
@@ -2,10 +2,10 @@
 import pytest
 import sqlalchemy as s
 
-
-def test_can_query_schema(new_blank_db):
+@pytest.mark.integration
+def test_can_query_schema(clean_db):
     # Simple smoke test to ensure schemas/tables exist and are empty
-    with new_blank_db.connect() as conn:
+    with clean_db.connect() as conn:
 
         count = conn.execute(s.sql.text("SELECT count(*) FROM augur_operations.config")).scalar()
         assert count == 0
