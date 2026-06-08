fcde27ac10d7d28034364681aeaaa9e5d74a5353
Adrian Edwards <adredwar@redhat.com>
2026-06-08 17:13:18 -0400

    revert keyman orchestrator back to regular environment variables so it doesn't import CollectOSS
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- keyman/Orchestrator.py
+++ keyman/Orchestrator.py
@@ -4,16 +4,15 @@ import random
 import time
 
 from keyman.KeyOrchestrationAPI import spec, WaitKeyTimeout, InvalidRequest
-from collectoss.application.environment import SystemEnv
 
-if SystemEnv.get("KEYMAN_DOCKER"):
+if os.environ.get("KEYMAN_DOCKER"):
     import sys
     import redis
     import logging
 
     sys.path.append("/collectoss")
 
-    conn = redis.Redis.from_url(SystemEnv.get("REDIS_CONN_STRING"))
+    conn = redis.Redis.from_url(os.environ.get("REDIS_CONN_STRING"))
 
     # Just log to stdout if we're running in docker
     logger = logging.Logger("KeyOrchestrator")
