306ba82318fdc98dceb8de27daf741e411c47cf7
Adrian Edwards <adredwar@redhat.com>
2026-06-23 16:43:03 -0400

    helpers for processing paths
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/application/paths.py
+++ collectoss/application/paths.py
@@ -1,5 +1,23 @@
 from collectoss.application.environment import SystemEnv
+from pathlib import Path
 
+def _verify_path(path: Path, create = True) -> Path:
+    """Verify the path is a valid directory"""
+    if create:
+        if not path.exists():
+            path.mkdir(parents=True)
+        if not path.is_dir():
+            raise ValueError(f"Path {path} is not a valid directory")
+    return path.resolve()
+
+
+def _path_from_env(env_value: str) -> Path:
+    """Get the path from the environment variable"""
+    if env_value is None:
+        return None
+    if env_value == "":
+        return None
+    return Path(env_value)
 
 class SystemPaths:
     """Enable consistent storage and retrieval of filesystem paths needed by the system"""
