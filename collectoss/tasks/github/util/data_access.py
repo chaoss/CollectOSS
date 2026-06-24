14cff851fd99db4a4438f24292f04ca9b2cc8239
Adrian Edwards <adredwar@redhat.com>
2026-06-24 16:02:23 -0400

    Documentation and specify type for keyclient arg
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/tasks/github/util/data_access.py
+++ collectoss/tasks/github/util/data_access.py
@@ -3,7 +3,14 @@ import logging
 class DataAccess:
     """Base class for shared HTTP data access functionality"""
     
-    def __init__(self, platform_name: str,logger: logging.Logger, key_client = None):
+    def __init__(self, platform_name: str,logger: logging.Logger, key_client:KeyClient = None):
+        """Initializes the DataAccess object
+
+        Args:
+            platform_name (str): the name primarily used to identify this platform's API limits to keyman. Example: "github_rest" or "github_graphql"
+            logger (logging.Logger): the logger to use for this class
+            key_client (KeyClient, optional): An instance of keyclient to use instead of creating one. Intended for testing purposes. Defaults to None (create a new one).
+        """
     
         self.logger = logger
         self.platform_name = platform_name
