85a5519cbeebc022ae4312592fcc94e17fcd0d04
Adrian Edwards <adredwar@redhat.com>
2026-06-24 15:56:16 -0400

    initialization for the shared DataAccess class
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/tasks/github/util/data_access.py
+++ collectoss/tasks/github/util/data_access.py
@@ -1,5 +1,11 @@
-
-
+from keyman.KeyClient import KeyClient
+import logging
 class DataAccess:
     """Base class for shared HTTP data access functionality"""
-    pass
+    
+    def __init__(self, platform_name: str,logger: logging.Logger, key_client = None):
+    
+        self.logger = logger
+        self.key_client = KeyClient(platform_name, logger) if key_client is None else key_client
+        self.key = None
+        self.expired_keys_for_request = []
