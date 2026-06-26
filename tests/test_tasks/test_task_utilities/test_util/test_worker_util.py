4bdca51a8d3f12ac920c808a4cade29046ccff81
Adrian Edwards <adredwar@redhat.com>
2026-06-26 14:57:09 -0400

    add and test a batching function
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- tests/test_tasks/test_task_utilities/test_util/test_worker_util.py
+++ tests/test_tasks/test_task_utilities/test_util/test_worker_util.py
@@ -2,7 +2,7 @@ import logging
 import pytest
 import sqlalchemy as s
 
-from collectoss.tasks.util.worker_util import remove_duplicates_by_uniques
+from collectoss.tasks.util.worker_util import remove_duplicates_by_uniques, batched
 
 logger = logging.getLogger(__name__)
 
@@ -29,5 +29,57 @@ def test_remove_duplicates_by_uniques():
     assert len(remove_duplicates_by_uniques([data_1, data_2], ["cntrb_id", "gh_user_id", "gh_login"])) == 2
 
 
+class TestBatched:
+    @pytest.mark.unit
+    def test_batched_evenly_divisible(self):
+        result = list(batched([1, 2, 3, 4, 5, 6], 3))
+        assert result == [[1, 2, 3], [4, 5, 6]]
 
+
+    @pytest.mark.unit
+    def test_batched_remainder(self):
+        result = list(batched([1, 2, 3, 4, 5], 3))
+        assert result == [[1, 2, 3], [4, 5]]
+
+
+    @pytest.mark.unit
+    def test_batched_smaller_than_batch_size(self):
+        result = list(batched([1, 2], 100))
+        assert result == [[1, 2]]
+
+
+    @pytest.mark.unit
+    def test_batched_empty(self):
+        result = list(batched([], 3))
+        assert result == []
+
+
+    @pytest.mark.unit
+    def test_batched_size_one(self):
+        result = list(batched([1, 2, 3], 1))
+        assert result == [[1], [2], [3]]
+
+
+    @pytest.mark.unit
+    def test_batched_consumes_generator_lazily(self):
+        consumed = []
+
+        def tracking_gen():
+            for i in range(6):
+                consumed.append(i)
+                yield i
+
+        gen = batched(tracking_gen(), 3)
+
+        next(gen)
+        assert consumed == [0, 1, 2], "should only have consumed first batch"
+
+        next(gen)
+        assert consumed == [0, 1, 2, 3, 4, 5]
+
+
+    @pytest.mark.unit
+    def test_batched_returns_lists(self):
+        result = list(batched(range(4), 2))
+        assert all(isinstance(chunk, list) for chunk in result)
     
