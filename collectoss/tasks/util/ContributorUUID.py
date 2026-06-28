afa1a1d229a5137b58feec923674756023cc3811
Adrian Edwards <adredwar@redhat.com>
2026-06-28 18:01:18 -0400

    move ForgePlatformType to somewhere even MORE generally reusable
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/tasks/util/ContributorUUID.py
+++ collectoss/tasks/util/ContributorUUID.py
@@ -2,14 +2,7 @@ from __future__ import annotations
 
 import uuid
 import typing
-from enum import Enum
-
-class ForgePlatformType(Enum):
-    """Identification values for different known forges.
-    Intended for helping identify the relevant API endpoints and tasks to use for collection"""
-    UNRESOLVEABLE = 0
-    GITHUB = 1
-    GITLAB = 2
+from collectoss.util.enums import ForgePlatformType
 
 class ContributorUUID:
     struct = {
