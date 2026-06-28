78658ca67d65a8e1d22126281fc4d6a17be7bb77
Adrian Edwards <adredwar@redhat.com>
2026-06-28 17:56:20 -0400

    docstring
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/tasks/util/ContributorUUID.py
+++ collectoss/tasks/util/ContributorUUID.py
@@ -5,6 +5,8 @@ import typing
 from enum import Enum
 
 class ForgePlatformType(Enum):
+    """Identification values for different known forges.
+    Intended for helping identify the relevant API endpoints and tasks to use for collection"""
     UNRESOLVEABLE = 0
     GITHUB = 1
     GITLAB = 2
